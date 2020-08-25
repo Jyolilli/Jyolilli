1ce824fd516657a7e970892bafbb650d0143f2dd
Jyoti <jyoti.contact@gmail.com>
2020-08-25 21:13:18 +0100

    apply styling

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,41 @@ import React from 'react';
 import '../App.css';
 
 function Main() {
-  return <div className="App">Main component</div>;
+  return (
+    <div className="main-component">
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+      <div className="card-header"></div>
+    </div>
+  );
 }
 
 export default Main;
