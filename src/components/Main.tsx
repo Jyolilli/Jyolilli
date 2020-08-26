7bc0bfb2b32b887195d91bef17cbdc9856c5ef95
Lilli Baaske <baaske@architrave.de>
2020-08-26 21:01:54 +0200

    Create html5 structure

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 
 function Main() {
   return (
-    <div className="main-component">
-      <div className="card">
-      <div className="card-profile-pic"></div>
-      <div className="card-emoji-icon"></div>
-      </div>
-      
-    </div>
+    <main className="main-component">
+      <section>
+        <article className="card">
+          <header>
+            <div className="profile-pic"></div>
+            <div className="emoji"></div>
+            <h2>Claas:</h2>
+          </header>
+          <div>
+            <p>just writing a placeholder post</p>
+          </div>
+          <footer>
+            <header>
+              <button>LikeBtn</button>
+              <button>ChatBtn</button>
+            </header>
+            <div>
+              <p>Hello, what a beautiful night at Codebar</p>
+              <p>Yep, isn't it!</p>
+              <p>Hi, what awesome evening at Codebar</p>
+              <form action="">
+                <input type="text" name="quickpost" />
+                <input type="submit" value="Post" />
+              </form>
+
+            </div>
+          </footer>
+        </article>
+      </section>
+    </main>
   );
 }
 
