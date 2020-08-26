29d2aff9a5afc5defd7ec3d9dd745a9119f15b0e
Lilli Baaske <baaske@architrave.de>
2020-08-26 22:17:11 +0200

    Get posts data from json

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,36 +1,24 @@
 import React from "react";
+import Card from './Card';
 import "../App.css";
+import posts from '../data.json';
+
+type posts = {
+  postOwner: string;
+  postContent: string;
+  type: string;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+
 
 function Main() {
   return (
     <main className="main-component">
       <section>
-        <article className="card">
-          <header>
-            <div className="profile-pic"></div>
-            <div className="emoji"></div>
-            <h2>Claas:</h2>
-          </header>
-          <div>
-            <p>just writing a placeholder post</p>
-          </div>
-          <footer>
-            <header>
-              <button>LikeBtn</button>
-              <button>ChatBtn</button>
-            </header>
-            <div>
-              <p>Hello, what a beautiful night at Codebar</p>
-              <p>Yep, isn't it!</p>
-              <p>Hi, what awesome evening at Codebar</p>
-              <form action="">
-                <input type="text" name="quickpost" />
-                <input type="submit" value="Post" />
-              </form>
-
-            </div>
-          </footer>
-        </article>
+        {posts.map((post) => (<Card {...post} />))}
+         
+        
       </section>
     </main>
   );
