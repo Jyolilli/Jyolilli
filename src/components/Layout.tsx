effc63ea76fbe26c4a867ca529038573669ae4dc
Lilli Baaske <baaske@architrave.de>
2020-09-08 16:04:20 +0200

    Add new layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <article className="layout">
+      <header className="square-pic-large"></header>
+      <main className="pic-small-container">
+        <div className="square-pic-small-one">one</div>
+        <div className="square-pic-small-two">two</div>
+        <div className="square-pic-small-three">three</div>
+        <div className="square-pic-small-four">four</div>
+        <div className="square-pic-small-five">five</div>
+        <div className="square-pic-small-six">six</div>
+      </main>
+      <footer></footer>
+    </article>
+  );
+};
+
+export default Layout;
